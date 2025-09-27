9645b488a719933cb03fbf8229a8b4a662c3c43d
Mykola Zelman <mykolazelman@gmail.com>
2025-09-27 21:05:14 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,29 @@
+function getElementWidth(content, padding, border) {
+    let contentNum = parseFloat(content);
+    let paddingNum = parseFloat(padding);
+    let borderNum = parseFloat(border);
+    return contentNum + paddingNum*2 + borderNum*2;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+function checkForSpam(message) {
+    let banWord1 = "spam";
+    let banWord2 = "sale";
+    let lowerMessage = message.toLowerCase();
+    if (lowerMessage.includes(banWord1) || lowerMessage.includes(banWord2)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
